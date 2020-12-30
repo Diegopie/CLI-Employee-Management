c24ed2e032ecce91b7163e8e55fd3a53cdbcec2f
Diego Hernandez <ishtaracademy@outlook.com>
2020-12-29 19:13:22 -0700

    Corrected schema and poulated seeds

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+Vanguard 
+Crucible
+
+-- Departments
+INSERT INTO department (name) VALUES ('Vanguard');
+INSERT INTO department (name) VALUES ('Crucible');
+INSERT INTO department (name) VALUES ('Gambit');
