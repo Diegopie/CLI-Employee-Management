c31ab362859a5c0557bb18e4052506f4c9c65117
Diego Hernandez <ishtaracademy@outlook.com>
2021-01-04 17:09:47 -0700

    Moved all prompts to a seperate file; view/add roles and departments are working

--- app.js
+++ app.js
@@ -1,91 +1,127 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const connection = require('./config/connection');
+const { prompts } = require('./config/prompts');
 require("dotenv").config();
 require("console.table");
 
-
 // * Get Case From UJer
 userCase();
-function userCase () {
-  inquirer
-    .prompt({
-      type: 'list',
-      name: 'userCase',
-      message: 'Select an Option',
-      choices: [
-        'View Employees',
-        'View Roles',
-        'View Departments',
-        'Add Employee',
-        'Add Role',
-        'Add Department',
-        'Update Employee',
-        'Quit',
-      ],
-    }) .then(({ userCase }) => {
-      switch (userCase) {
-        case 'View Employees':
-          viewEmployees();
-          break;
-        case 'View Roles':
-          viewRoles();
-          break;
-        case 'View Departments':
-          viewDep();
-          break;
-        case 'Add Employee':
-          addEmployee();
-          break;
-        case 'Add Role':
-          addRole();
-          break;
-        case 'Add Department':
-          addDep();
-          break;
-        case 'Update Employee':
-          updateEmployee();
-          break;
-        case 'Quit':
-          quit();
-          break;
-      }
-    })
+function userCase() {
+    inquirer
+        .prompt(prompts.case)
+        .then(({ userCase }) => {
+            switch (userCase) {
+                case 'View Employees':
+                    viewEmployees();
+                    break;
+                case 'View Roles':
+                    viewRoles();
+                    break;
+                case 'View Departments':
+                    viewDep();
+                    break;
+                case 'Add Employee':
+                    addEmployee();
+                    break;
+                case 'Add Role':
+                    addRole();
+                    break;
+                case 'Add Department':
+                    addDept();
+                    break;
+                case 'Update Employee':
+                    updateEmployee();
+                    break;
+                case 'Quit':
+                    break;
+            }
+        })
 };
 
 
 function viewEmployees() {
-  console.log('viewEmployees');
+    console.log('viewEmployees');
 };
 
 function viewRoles() {
-  console.log('viewRoles');
+    console.log('viewRoles');
+    connection.query('Select * from role', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        userCase();
+    })
 };
 
 function viewDep() {
-  console.log('viewDep');
+    console.log('viewDep');
+    connection.query('Select * from department', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        userCase();
+    })
 };
 
 function addEmployee() {
-  console.log('addEmployee');
+    console.log('addEmployee');
+    inquirer
+        .prompt(prompts.addEmp)
+        .then((res) => {
+            connection.query(
+                `INSERT INTO employee 
+                    (first_name, last_name, role_id, manager_id)
+                VALUE
+                    ("${res.first_name}",
+                    "${res.last_name}",
+                    "${res.role_id}",
+                    "${res.manager_id}")
+      `, (err, res) => {
+                if (err) throw err;
+                console.table(res);
+                userCase();
+            })
+        })
 };
 
 function addRole() {
-  console.log('addRole');
+    console.log('addRole');
+    inquirer
+        .prompt(prompts.addRole)
+        .then((res) => {
+            connection.query(
+                `INSERT INTO role 
+                    (title, salary, department_id)
+                VALUE
+                    ("${res.title}",
+                    "${res.salary}",
+                    "${res.department_id}")
+      `, (err, res) => {
+                if (err) throw err;
+                console.table(res);
+                userCase();
+            })
+        })
 };
 
-function addDep() {
-  console.log('addDep');
-};
-
-function someFunc6() {
-  console.log('someFunc6');
+function addDept() {
+    console.log('addDept');
+    console.log('addRole');
+    inquirer
+        .prompt(prompts.addDept)
+        .then((res) => {
+            connection.query(
+                `INSERT INTO department 
+                    (name)
+                VALUE
+                    ("${res.name}")
+      `, (err, res) => {
+                if (err) throw err;
+                console.table(res);
+                userCase();
+            })
+        })
 };
 
 function updateEmployee() {
-  console.log('updateEmployee');
-};
-
-function quit() {
-  console.log('quit');
+    console.log('updateEmployee');
 };
---
+++ config/prompts.js
@@ -0,0 +1,95 @@
+const prompts = {
+    case: {
+        type: 'list',
+        name: 'userCase',
+        message: 'Select an Option',
+        choices: [
+            'View Employees',
+            'View Roles',
+            'View Departments',
+            'Add Employee',
+            'Add Role',
+            'Add Department',
+            'Update Employee',
+            'Quit',
+        ],
+    },
+
+    addEmp: [
+        {
+            type: 'input',
+            message: "Enter Employee's First Name",
+            name: 'first_name'
+        },
+        {
+            type: 'input',
+            message: "Enter Employee's Last Name",
+            name: 'last_name'
+        },
+        {
+            type: 'input',
+            message: "Enter Role ID Employee's Title (Select View Roles in the Main Menu to Find Role ID's",
+            name: 'role_id',
+            validate: (inp) => {
+                check = parseInt(inp);
+                console.log(typeof(check));
+                // if (check !== Number) {
+                //     return "Please Enter a Number"
+                // }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            message: "Enter ID of Employee's Manager",
+            name: 'manager_id',
+            validate: (inp) => {
+                check = parseInt(inp);
+                // if (check !== Number) {
+                //     return "Please Enter a Number"
+                // }
+                return true;
+            }
+        },
+    ],
+
+    addRole: [
+        {
+            type: 'input',
+            message: "Enter New Title",
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: "Enter Salary for this Role",
+            name: 'salary'
+        },
+        {
+            type: 'input',
+            message: "Enter Department ID for this Role",
+            name: 'department_id',
+            
+        },
+    ],
+
+    addDept: {
+        type: 'input',
+        message: "Enter New Department Name",
+        name: 'name'
+    },
+
+    updateEmployee: [
+        {
+            type: 'input',
+            message: "Enter ID of Employee to Update",
+            name: 'employee_id'
+        },
+        {
+            type: 'input',
+            message: "Enter ID of Their New Role",
+            name: 'role_id'
+        },
+    ]
+}
+
+exports.prompts = prompts;
